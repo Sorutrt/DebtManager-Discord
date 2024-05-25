b7cdb55e77adbe9f40f4dc161fa0bfd47df11829
sorutrt <kokishiono.58@gmail.com>
2024-05-25 14:10:57 +0900

    command implementation

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,29 @@
-import { GatewayIntentBits, Partials, Message, Client } from 'discord.js'
-import dotenv from 'dotenv'
+import { GatewayIntentBits, Partials, Message, Client, Events, CacheType, Interaction } from 'discord.js';
+import dotenv from 'dotenv';
+import { pingData, pingFunc } from "./commands/ping";
 
-dotenv.config()
+dotenv.config();
 
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMembers,
         GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
+        GatewayIntentBits.MessageContent,
     ],
     partials: [
         Partials.Message,
         Partials.Channel
     ]
-})
+});
 
-client.once('ready', () => {
+client.once('ready', async () => {
     console.log('Ready!')
     console.log(client.user?.tag)
-})
+    if (client.user) {
+        module.exports = client.user.tag;
+    }
+});
 
 client.on('messageCreate', async (message: Message) => {
     if (message.author.bot) return
@@ -27,6 +31,27 @@ client.on('messageCreate', async (message: Message) => {
         message.channel.send('Pong!')
     }
     console.log(message.content);
+});
+
+// slash commands
+client.on(Events.InteractionCreate, async (interaction: Interaction<CacheType>) => {
+    //console.log(interaction); //test code
+    
+    if (!interaction.isCommand()) {
+        return;
+    }
+    const { commandName } = interaction;
+
+    if ( commandName === pingData.name ) {
+        try {
+            await pingFunc(interaction);
+        }
+        catch(e) {
+            console.log('main.ts error')
+            console.error(e);
+        }
+    }
 })
+
 
 client.login(process.env.TOKEN)
